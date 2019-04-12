de3f494c75d9764300fd5d0170066b4ac2e5d9dd
Vagner Sabadi <vagnerbuzatta@hotmail.com>
2019-04-11 23:36:07 -0300

    Chips categorias

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:booksky/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Booksy',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
